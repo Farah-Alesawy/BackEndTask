8fc9b852c131501a0d825c560aeb86c5b23730a0
Farah-Alesawy <farahmed747@gmail.com>
2025-12-18 21:13:44 +0200

    Add files via upload

---
+++ ShortestStrignP/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ShortestStringProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "code", "backend", "ai", "circle" };
+
+            string answer = FindShortestString(stringArray);
+
+            Console.WriteLine($"Output: {answer}");
+
+        }
+
+        static string FindShortestString(string[] stringArray)
+        {
+            string shortest = stringArray[0];
+
+            foreach (string item in stringArray)
+            {
+                if (item.Length < shortest.Length)
+                {
+                    shortest = item;
+                }
+            }
+
+            return shortest;
+        }
+    }
+}
